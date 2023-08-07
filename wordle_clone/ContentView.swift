5e27dee7e0e1bd5d2bf8a6d0589e71196e123347
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-07 09:25:00 -0400

    Initial Commit

---
+++ wordle_clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wordle_clone
+//
+//  Created by Ethan Shafran Moltz on 8/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wordle_clone/wordle_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordle_cloneApp.swift
+//  wordle_clone
+//
+//  Created by Ethan Shafran Moltz on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct wordle_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
