9b64efa9c8821afe202f07ba4ed0fcbcb8b6dc2a
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-07 22:34:39 -0400

    clean up

---
+++ wordle_clone/Views/WordleView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct WordleView: View{
+   
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+
+struct WordleView_Preview: PreviewProvider {
+    static var previews: some View {
+        WordleView()
+        
+    }
+}
