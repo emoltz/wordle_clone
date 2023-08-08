9e8ea89bcb2fa5dbe1fbb412dec30373d6eeff47
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-08 10:42:27 -0400

    grid

--- wordle_clone/Components/GameBoardView.swift
+++ wordle_clone/Components/GameBoardView.swift
@@ -3,8 +3,12 @@ import SwiftUI
 struct GameBoardView: View {
     @State private var characters: [String] = Array(repeating: "W", count: 25)
     var body: some View {
-        VStack{
-            Text("Hello")
+        let columns = Array(repeating: GridItem(.flexible()), count: 5)
+
+        LazyVGrid(columns: columns, spacing: 20){
+            ForEach(characters.indices, id: \.self) { index in
+                            Tile(tileColor: Color("TileColor"), character: $characters[index])
+                        }
         }
         
     }
