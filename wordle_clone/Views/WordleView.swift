f4d4ac84c3ae1487fed1984c829d213f54688e24
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-09 10:12:19 -0400

    words go in each row now

--- wordle_clone/Views/WordleView.swift
+++ wordle_clone/Views/WordleView.swift
@@ -3,35 +3,49 @@ import SwiftUI
 struct WordleView: View{
     @State private var characters: [String] = Array(repeating: "", count: 5)
     @State private var currentInput: String = ""
+    @State private var currentRow: Int = 0
+    @State private var rows: [[String]] = Array(repeating: Array(repeating: "", count: 5), count: 5)
     
     var body: some View {
         
         VStack{
-            GameBoardView(characters: $characters)
-                .padding()
+            ForEach(rows.indices, id:\.self){ i in
+                GameBoardRow(characters: $rows[i])
+            }
+            .padding()
+            
+            
             Spacer()
+            
             OnScreenKeyboard(currentInput: $currentInput)
+            
+            Button("Guess!") {
+                print("Pressed")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color("TileColor"))
+            .cornerRadius(10)
+            .fontWeight(.bold)
+            
         }
         .onChange(of: currentInput){ newValue in
             processInput(newValue)
             
         }
- 
+        
     }
     
-    private func processInput(_ input: String){
-        // the point of the below line is to find the place with the "" and get me that index
-        // `guard`makes sure that this works before moving on to the rest of the code
-        guard let index = characters.firstIndex(where: {$0.isEmpty}) else {
-            print("no more room!")
-            return
+    private func processInput(_ input: String) {
+            guard currentRow < rows.count, let index = rows[currentRow].firstIndex(where: { $0.isEmpty }) else {
+                print("no more room!")
+                return
+            }
+            rows[currentRow][index] = input
+            print("Characters: \(rows[currentRow])")
             
+            currentInput = ""
         }
-        characters[index] = input
-        print("Characters: \(characters)")
-        
-        currentInput = ""
-    }
 }
 
 
