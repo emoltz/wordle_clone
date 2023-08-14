6d2c4c9feacf3c63e6e9b49b4f2110eeaef675cd
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-14 12:15:23 -0400

    firebase init

--- wordle_clone/wordle_cloneApp.swift
+++ wordle_clone/wordle_cloneApp.swift
@@ -1,14 +1,18 @@
-//
-//  wordle_cloneApp.swift
-//  wordle_clone
-//
-//  Created by Ethan Shafran Moltz on 8/7/23.
-//
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct wordle_cloneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
